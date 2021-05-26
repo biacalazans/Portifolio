bfb1e707a82070b1601bc25b40727cb47c687300
Beatriz Calazans <beatriz.calazans@hotmail.com>
2021-05-25 22:54:39 -0300

    Add content

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { makeStyles } from "@material-ui/core/styles";
 import NavBar from "./components/NavBar";
+import DevFocusImg from "./assets/undraw_dev_focus.svg";
 
 const useStyles = makeStyles((theme) => ({
   sectionDesktop: {
@@ -24,8 +25,27 @@ function App() {
     // <div className={classes.sectionDesktop}>
     <div className="app">
       <NavBar></NavBar>
+      <div className="main-container">
+        <div className="flex-container">
+          <div className="section">
+            <p className="highlight big">
+              Estudante, desenvolvedora e programadora
+            </p>
+            <p className="info big">
+              Curabitur dictum magna arcu, et semper enim consectetur sit amet.
+              Etiam pellentesque tellus turpis, a sollicitudin neque faucibus
+              dignissim. Donec et pellentesque nulla. Nulla mattis maximus massa
+              nec commodo. Class aptent taciti sociosqu ad litora torquent per
+              conubia nostra, per inceptos himenaeos. Phasellus cursus nisl id
+              odio fermentum.
+            </p>
+          </div>
+          <div>
+            <img className="dev-img" src={DevFocusImg}></img>
+          </div>
+        </div>
+      </div>
     </div>
-    // </div>
   );
 }
 
