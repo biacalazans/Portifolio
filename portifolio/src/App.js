3a09d1c93b825021e889c30229c803cf8697034c
Beatriz Calazans <beatriz.calazans@hotmail.com>
2021-08-15 20:21:46 -0300

    Update app

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -1,34 +1,13 @@
-import "./App.css";
-
-import NavBar from "./components/NavBar";
-import { Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 
-
+import { Home } from "./Pages/index";
 
 function App() {
-  
-
   return (
-    // <div className={classes.sectionDesktop}>
-    // <div className="app">
-    //   <NavBar></NavBar>
-      
-    //   <div className="main-container">
-    //     <div className="flex-container">
-    //       <div className="section">
-    //         <p className="highlight big">
-    //           Estudante, desenvolvedora e programadora
-            
-          
-    //     </div>
-    //   </div>
-    //   </div>
-    // </div>
-    <div>
-      <NavBar/>
-   </div> 
-      
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   );
 }
 
