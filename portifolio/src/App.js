fc6fcdf211b742a0d19b4b22fd99a9806a8b2f91
Beatriz Calazans <beatriz.calazans@hotmail.com>
2022-03-28 12:46:17 -0300

    test

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import React from "react";
 import AppRoutes from "./routes";
-import { Home } from "./Pages/main/index";
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <AppRoutes />
-    // </BrowserRouter>
-    <Home />
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
