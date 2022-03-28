5219ec559b7c17c866661997a5f17ba995d9a695
Beatriz Calazans <beatriz.calazans@hotmail.com>
2022-03-28 12:31:55 -0300

    test without routes

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import React from "react";
 import AppRoutes from "./routes";
+import { Home } from "./Pages/main/index";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <AppRoutes />
+    // </BrowserRouter>
+    <Home />
   );
 }
 
